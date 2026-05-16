ad714f3b6ddc785f6aac52934c23228df0a36fa0
Arnav Tiwari <arnavt292@gmail.com>
2026-05-17 02:41:24 +0530

    Updated render.yaml file

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,6 +1,10 @@
+// backend/src/db/index.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
